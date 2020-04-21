73d1bc6e3fa381c5be7c96623db95707b389d81c
tan-jia-wen <64011431+tan-jia-wen@users.noreply.github.com>
2020-04-21 21:05:35 +0800

    prompt for height and weight and print
    System.out.println("Enter height: "); - prompt user to enter height
    System.out.println("Your height is " + height); - print out user's height
    System.out.println("Enter weight: "); - prompt user to enter weight
    System.out.println("Your weight is " + weight); - print out user's weight

--- Question2.java
+++ Question2.java
@@ -11,6 +11,13 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.println("Enter height: ");
     double height = in.nextDouble();
+    System.out.println("Your height is " + height);
+
+    Scanner in2 = new Scanner(System.in);
+    System.out.println("Enter weight: ");
+    double weight = in2.nextDouble();
+    System.out.println("Your weight is " + weight);
   }
 }
