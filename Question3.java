08b63db7c1ed4a0f8eed2b2c517d8a7e259cdbc2
tan-jia-wen <64011431+tan-jia-wen@users.noreply.github.com>
2020-04-21 22:20:16 +0800

    Read and multiplies user input, then prints results
    System.out.println("Enter integer: "); - prompts user to enter integer
    int integer = in.nextInt(); - reads user input
    int result = integer * integer; - multiplies user input
    System.out.printIn("Result: " + result); - prints out final result
    in.close(); - close scanner

--- Question3.java
+++ Question3.java
@@ -11,6 +11,11 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+    System.out.println("Enter integer: ");
+    int integer = in.nextInt();
+    int result = integer * integer;
+    System.out.printIn("Result: " + result);
+
+    in.close();
   }
 }
