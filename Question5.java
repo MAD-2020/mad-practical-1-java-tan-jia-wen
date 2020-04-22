0be35faeceee30f7b17cf1df5a9a011d4f40c8eb
tan-jia-wen <64011431+tan-jia-wen@users.noreply.github.com>
2020-04-22 15:50:56 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Question5
 {
@@ -27,6 +29,30 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Number of integers: "); //prompt user for number of integers
+    int numOfInt = in.nextInt(); //read user input
+    ArrayList<Integer> numList = new ArrayList<Integer>(); //create empty list
+    while (numList.size() != numOfInt){ //condition: no. of elements in list not the same as no. of integers 
+      String input = String.format("Number #%: ", numList.size()+1); //format for prompt
+      System.out.print(input); //prompt user for integers
+      int integer = in.nextInt(); //read user inputs
+      numList.add(integer); //add user inputs to list
+    }
+    integer[] array = numList.toArray(new Integer[0]); //create array from list
+    HashMap<integer, integer> hash = new HashMap<integer, integer>(); //create hashmap object
+    int highestCount = 1;
+    int highestValue = 0;
+    for (int i = 0; i < numList.size(); i++){
+      if (hash.get(array[i]) != null){
+        int count = hash.get(array[i]); //get value of key
+        count++; //increase by 1
+        hash.put(array[i].count); //put key+value into hashmap
+        if (count > max){
+          highestCount = count;
+          highestValue = array[i]; //set new value
+        }
+      }
+    }
+    System.out.print("Number with the highest occurrence: " + max);
   }
 }
